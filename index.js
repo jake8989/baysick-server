71d5f6ecfef89bf8cdacb140536abc8341f2bf61
jake <jjk848798@gmail.com>
2023-06-08 03:09:58 +0530

    cors:error

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const port = process.env.PORT || 8080;
 const cors = require('cors');
 
 app.use(cors());
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
+	next();
+});
 connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
